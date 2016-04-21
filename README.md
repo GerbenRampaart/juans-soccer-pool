cfc3f48fc7c99b2b6111f990ae36942c73357441
Gerben Rampaart <Gerben Rampaart>
2016-04-21 23:16:41 +0200

    added plumbing

---
+++ README.md
@@ -0,0 +1,3 @@
+This is an open source web application for having a soccerpool with your friends.
+
+Goal is to support the WC, EC, CL and EL.
---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "angular2/core";
+
+@Component({
+    selector: 'app',
+    template: 'some app'
+})
+export class AppComponent {
+    
+}
